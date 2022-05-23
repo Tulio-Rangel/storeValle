d320cf4994cd426a2714af897a3c23e3a941d7d6
Tulio-Rangel <tjrn20@hotmail.com>
2022-05-23 16:52:45 -0500

    Agregado el jwt en el login con su respectivo secret

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const { config } = require('../config/config')
 
 router.post('/login',
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role
+      }
+      const token = jwt.sign(payload, config.jwtSecret);
+      res.json({
+        user,
+        token
+      });
     }
     catch (error) {
       next(error);
